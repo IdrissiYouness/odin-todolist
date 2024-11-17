95937b6c35cf9782c2ec0da865222a96c500bbbb
Youness Sghiouri Idrissi <zahirkamarul@outlook.com>
2024-11-17 21:36:04 +0100

    start preparing modal forms

---
+++ src/ui/modals.js
@@ -0,0 +1,35 @@
+import { createDiv, createBtn} from "./dom-elements";
+import { createTextInputHolder,
+         createTextAreaHolder,
+         createDateInputHolder } from "./input-holders";
+
+
+const createProjectModal = () => {
+
+    const modal = createDiv("modal-container");
+    modal.setAttribute("id","add-project-modal");
+
+    const modalBox = createDiv("modal-box");
+
+    const form = document.createElement('form');
+    form.setAttribute('action', '#');
+
+    const projectNameInput = createTextInputHolder(
+        "title","Project name",
+        "title-input","Enter project name..."
+    );
+
+    const confirmModalButton = createBtn("confirm-add-btn");
+
+    modal.appendChild(modalBox);
+    modalBox.appendChild(form);
+    form.appendChild(projectNameInput);
+    form.appendChild(confirmModalButton);
+
+    return {
+        modal,
+        projectNameInput,
+        projectColorInput,
+        createProjectBtn,
+    };
+};
