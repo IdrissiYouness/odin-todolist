188494b61820c8cfe0a24cff617c0b01c9e9ee73
Youness Sghiouri Idrissi <zahirkamarul@outlook.com>
2024-11-25 19:43:32 +0100

    handle description toggle

--- src/events/event-listeners.js
+++ src/events/event-listeners.js
@@ -1,21 +1,27 @@
-import { handleOpenModal, handleCloseModal} from './modal-event-handlers.js';
+import { handleOpenModal, handleCloseModal } from './modal-event-handlers.js';
 
-export function attachEventListeners(){
+export function attachEventListeners() {
 
-    document.addEventListener('click',(event)=>{
+
+
+    document.addEventListener('click', (event) => {
 
         const target = event.target;
 
-        if (target.classList.contains('open-modal-btn')){
+
+        if (target.classList.contains('open-modal-btn')) {
             const modalId = target.getAttribute('data-modal-id');
-            handleOpenModal(modalId)
+            handleOpenModal(modalId);
         }
+
 
         if (target.classList.contains('close-modal-btn')) {
             const modal = target.closest('.modal-container');
             handleCloseModal(modal);
         }
 
-
-    })
+        if (target.classList.contains('task-holder') || target.classList.contains('task-title')){
+            target.closest('.task-giant-item').classList.toggle('expanded');
+        }
+    });
 }
