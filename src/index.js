b755a566978355eba82075298c3fa2befca9fdbc
Youness Sghiouri Idrissi <zahirkamarul@outlook.com>
2024-11-26 18:46:17 +0100

    Render everything from index & add task and project event creation

--- src/index.js
+++ src/index.js
@@ -1,108 +1,177 @@
 import './styles/style.css';
-import { createDiv } from './ui/dom-elements';
-import createNav, {renderProjectsToNav,getActiveProjectId} from "./ui/nav";
-import createMainContent ,{renderTasks} from './ui/main-content';
+import createNav from './ui/nav';
+import createMainContent ,{createTaskItem,addTaskToDom} from './ui/main-content.js';
+import { createDiv } from './ui/dom-elements.js';
+import { getActiveProjectId,createProjectTab,addProjectToNav,createDefaultProject } from "./ui/nav";
 import { createProjectModal,createTaskModal} from "./ui/modals";
 import { attachEventListeners } from './events/event-listeners';
 import { importDataFromStorage,exportDataToStorage } from './utils/local-storage.js';
-
 import task from './modules/task.js';
 import project from './modules/project.js';
-import projectCollection from './modules/project-collection.js';
+import  createProjectCollection from './modules/project-collection.js';
 
+const {modal:projectModal,
+     projectNameInput,
+     confirmProjectButton } = createProjectModal();
 
-const mainContent = createDiv("main-container");
-const navigation = createNav();
+const {modal:taskModal,
+     titleTaskInput,
+     descInput,
+     priorityListSelect,
+     dueDateInput,
+     confirmTaskButton } = createTaskModal();
+
+const pageContainer = createDiv('main-container');
+const nav = createNav();
 const main = createMainContent();
 
-const { modal: projectModal,projectNameInput,confirmModalButton:confirmProjectBtn } = createProjectModal();
-const { modal: taskModal,titleTaskInput,descInput,priorityListSelect,dueDateInput,confirmModalButton:confirmTaskBtn } = createTaskModal();
-
-main.appendChild(projectModal);
 main.appendChild(taskModal);
+main.appendChild(projectModal);
 
-mainContent.appendChild(navigation);
-mainContent.appendChild(main);
-document.body.appendChild(mainContent);
+pageContainer.appendChild(nav);
+pageContainer.appendChild(main);
 
-
-const myProjects = projectCollection();
-importDataFromStorage(myProjects);
-
-if (myProjects.findProjectName("Default") === -1) {
-     const defaultProj = project('default', 'Default');
-     myProjects.addProject(defaultProj);
-     exportDataToStorage(myProjects);
- }
-
- renderProjectsToNav(myProjects);
- renderTasks(myProjects.getProject(0));
-
+document.body.appendChild(pageContainer);
 
 document.addEventListener('DOMContentLoaded', () => {
      attachEventListeners();
  });
 
 
- //using Date to generate a unique Id
+const myProjects = initializeStorage();
+
+renderProjects(myProjects);
+
+function initializeStorage() {
+     const newProjectCollection = createProjectCollection();
+     importDataFromStorage(newProjectCollection);
+     return newProjectCollection;
+}
 
 
-confirmProjectBtn.addEventListener('click',(event)=>{
-     event.preventDefault();
-     const newProj = project(Date.now().toString(), projectNameInput.value);
+
+ confirmProjectButton.addEventListener('click',()=>{
+     const projectName = projectNameInput.value;
+     const newId = Date.now().toString();
+
+     const newProj = project(newId,projectName);
      myProjects.addProject(newProj);
+     const newProjectTab = createProjectTab(newId,projectName);
+     addProjectToNav(newProjectTab);
+     newProjectTab.addEventListener
      exportDataToStorage(myProjects);
-     renderProjectsToNav(myProjects);
-     /*
-     createAndAddProjectToNav(newProj);
-     */
-})
+     return true;
+});
 
+confirmTaskButton.addEventListener('click',()=>{
 
-confirmTaskBtn.addEventListener('click',(event)=>{
-     event.preventDefault();
-
-     const newTask = task(Date.now().toString(),
-     titleTaskInput.value,
-     descInput.value,
-     priorityListSelect.value,
-     dueDateInput.value);
+     const newTaskId = Date.now().toString();
 
      const activeProjectId = getActiveProjectId();
 
-     const activeProject = myProjects.getProject(
-          myProjects.findProjectIndex(activeProjectId)
-      );
+     const activeProject = myProjects.findProjectIndex(activeProjectId);
 
-     activeProject.addATask(newTask);
-     exportDataToStorage(myProjects);
+     const newTask = task
+     (
+          newTaskId,
+          titleTaskInput.value,
+          descInput.value,
+          priorityListSelect.value,
+          dueDateInput.value
+     );
 
-     // using render tasks temporary to fix  undefined task info bug
-     renderTasks(activeProject);
+     myProjects.getProject(activeProject).addATask(newTask);
 
-     /*
-     const taskItem = createTaskItem(newTask);
+     const taskItem = createTaskItem(
+          newTaskId,
+          titleTaskInput.value,
+          descInput.value,
+          priorityListSelect.value,
+          dueDateInput.value
+     );
+
      addTaskToDom(taskItem);
-     */
+
+     exportDataToStorage(myProjects);
 
 })
 
+
+
 const tabs = document.querySelectorAll('.tab');
+
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
 
     tabs.forEach(tab => tab.classList.remove('active'));
 
+    const activeProjectId = tab.dataset.projectId;
+
+    renderTasks(activeProjectId);
 
     tab.classList.add('active');
 
-    const activeProjectId = tab.dataset.projectId;
-    const activeProject = myProjects.getProject(
-           myProjects.findProjectIndex(activeProjectId)
-    );
-    renderTasks(activeProject);
+    console.log('rendering the tasks of',activeProjectId);
   });
 });
+
+
+
+
+
+
+
+
+function renderTasks(projectId) {
+
+     const taskListContainer = document.querySelector(".task-list-container");
+     taskListContainer.innerHTML = "";
+
+     const projectIndex = myProjects.findProjectIndex(projectId);
+     const TaskListByProjectIndex = myProjects.getProject(projectIndex).getTasks();
+
+     TaskListByProjectIndex.forEach((task)=>{
+         const newTaskItem = createTaskItem(
+             task.id,
+             task.title,
+             task.description,
+             task.priority,
+             task.dueDate
+         );
+         addTaskToDom(newTaskItem);
+     });
+
+ }
+
+
+function renderProjects(projectCollection) {
+     const container = document.querySelector('.projects-container');
+     container.innerHTML = "";
+     projectCollection.printAll().forEach((newProject) => {
+
+          if(newProject.id === "default"){
+
+               const defaultProj = createDefaultProject(
+                    newProject.id,
+                    newProject.name
+                );
+               addProjectToNav(defaultProj);
+
+               newProject.tasks.forEach(() => {
+                    renderTasks(newProject.id);
+               });
+
+          }else{
+               const projectTab =  createProjectTab(newProject.id,newProject.name);
+               addProjectToNav(projectTab);
+          }
+
+     });
+ }
+
+
+
+
 
 
 
